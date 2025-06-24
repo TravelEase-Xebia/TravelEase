905279ee3ab474429007169bdc36cf3668cbcc6e
Sujal Bhandari <116736283+whysujalwhy@users.noreply.github.com>
2025-06-24 16:33:26 +0530

    Added booking micro-service

---
+++ models/booking.js
@@ -0,0 +1,20 @@
+const mongoose = require("../db");
+
+const BookingSchema = new mongoose.Schema({
+  departure: {
+    type: String,
+    required: true
+  },
+  destination: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  }
+});
+
+const Booking = mongoose.model("Booking", BookingSchema);
+
+module.exports = Booking;
