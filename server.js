fb5310fbffc251db650784f833020280c82e0cf8
Bhavesh-Kapur <Bhavesh.105635@stu.upes.ac.in>
2025-06-26 23:48:36 +0530

    payment microservice + the docker file

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Payment = require("./models/payment.js");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+
+app.post("/api/payment", async(req,res)=> {
+    const { departure, destination, date, flight, tickets, total } = req.body;
+
+  try {
+    const newPayment = await Payment.create({ departure, destination, date, flight, tickets, total });
+    res.status(201).json({ message: 'Booking successful', payment: newPayment });
+  } catch (error) {
+    res.status(500).json({ message: 'Booking failed', error });
+  }
+});
+
+
+
+
+app.listen(process.env.PORT || 3001 , () => {
+  console.log("payment microservice is running.");
+});
