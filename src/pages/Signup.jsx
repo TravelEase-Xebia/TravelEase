a337cb2fdebc9d16003bc89a23fa3bc0a216dead
kavya-singh101 <madhujeet111@gmail.com>
2025-06-29 02:15:08 +0530

    adding port env

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -31,7 +31,7 @@ const Signup = () => {
         password: formData.password,
        }
         try{
-          const response = await fetch(`${import.meta.env.VITE_LOGIN_API_URL}:${import.meta.env.VITE_LOGIN_PORT}/auth/register`, {
+          const response = await fetch(`${import.meta.env.VITE_LOGIN_API_URL}:5050/auth/register`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
