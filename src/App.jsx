5a9d3e7cc2ea929e54c5aec441b86f0020802ca3
Bhavesh-Kapur <Bhavesh.105635@stu.upes.ac.in>
2025-07-09 22:19:59 +0530

    landing

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup"; // If you have a signup page
 import Payment from "./pages/Payment";
 import FlightSearch from "./pages/FlightSearch";
+import Landing from "./pages/Landing";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
          <Route path="/payment" element={<Payment />} />
